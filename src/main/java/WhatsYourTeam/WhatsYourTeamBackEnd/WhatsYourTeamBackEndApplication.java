744a8695e92f75f70e9d931d64a999c303cee27f
kitaee <dlrlxo999@naver.com>
2022-11-29 14:30:24 +0900

    🚚 [SETTING] : Initial Setting

---
+++ src/main/java/WhatsYourTeam/WhatsYourTeamBackEnd/WhatsYourTeamBackEndApplication.java
@@ -0,0 +1,13 @@
+package WhatsYourTeam.WhatsYourTeamBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsYourTeamBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsYourTeamBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WhatsYourTeam/WhatsYourTeamBackEnd/WhatsYourTeamBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package WhatsYourTeam.WhatsYourTeamBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsYourTeamBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
